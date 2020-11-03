a3e7aaf376d85497046bc727a302b4e35f8e596c
Kyle Tanaka <ktanaka2714@gmail.com>
2020-11-03 13:26:23 -0800

    revised example

--- ProceduralExample/Program.cs
+++ ProceduralExample/Program.cs
@@ -6,7 +6,14 @@ namespace ProceduralExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!");
+            Console.WriteLine("Entre multiple words: ");
+            string input = Console.ReadLine();
+            string [] output = input.Split(' ');
+            foreach(string s in output){
+                Console.Write(s[0]);
+            }
+            Console.WriteLine();
         }
     }
 }
